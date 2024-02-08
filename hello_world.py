7f016c07a0cba96f7299f1e9ad3c0e7853118b2c
Ajay Kumar <this.ajk@gmail.com>
2024-02-08 14:44:34 +0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+"This is my first python program"
+print("Hello Python interpreter!")
+print("Hello Python world!")
+
+message = "Message: Hello Python world" #variable
+print(message)
+
+
+# print = "Print" # illigal to use python keyword as variable
+# print(print) #illigal
+
+
