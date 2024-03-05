b2ee3468df64c6ef6f739d1fb8ef7394496b6a59
Ajay Kumar <this.ajk@gmail.com>
2024-03-05 17:15:33 +0800

    Commit

--- hello_world.py
+++ hello_world.py
@@ -9,3 +9,44 @@ print(message)
 # print(print) #illigal
 
 
+message_1 = 'I told my friend, "Python is my favorite language!"'
+message_2 = "The language 'Python' is named after Monty Python, not the snake."
+message_3 = "One of Python's strengths is its diverse and supportive community."
+
+print(message_1)
+print(message_2)
+print(message_3)
+
+
+name = "ajay kumar"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ajay"
+last_name = "kumar"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+print("\tPython")
+
+favorite_language_1 = '     JULIA '
+
+import this
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+print(bicycles[1])
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles) 
+
+print(motor)
